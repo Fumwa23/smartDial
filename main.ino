1a97b525a62ed5d0c19ded1de7f7e47ac6e4ff20
NicolasRyj <139501733+NicolasRyj@users.noreply.github.com>
2024-01-01 15:05:47 +0100

    Worked on light fade function

--- main.ino
+++ main.ino
@@ -7,5 +7,20 @@
 //------------------------------Components---------------------------------
 // AS5600: https://www.mouser.sg/datasheet/2/588/AS5600_DS000365_5_00-1877365.pdf
 // ESP32:  https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf
+// 74HC595:https://www.instructables.com/Shift-Register-LED-Array-Arduino-5-Examples/
 // 
 //Third change
+
+void fadeLightFunction() {
+    /*
+    Create smooth gradient effect as the nob is turned around.
+    */
+}
+
+void setup() {
+
+}
+
+void loop() {
+  // Main loop code goes here
+}
