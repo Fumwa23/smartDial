e46c5e287e04e03b79c05481e563ddad527dbed1
NicolasRyj <139501733+NicolasRyj@users.noreply.github.com>
2024-01-01 11:12:05 +0100

    Third change

--- main.ino
+++ main.ino
@@ -7,3 +7,4 @@
 // AS5600: https://www.mouser.sg/datasheet/2/588/AS5600_DS000365_5_00-1877365.pdf
 // ESP32:  https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf
 // 
+//Third change
