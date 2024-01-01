237c5f668c28028a093e653a7e0c5db879b4110e
NicolasRyj <139501733+NicolasRyj@users.noreply.github.com>
2024-01-01 11:05:40 +0100

    Added components

--- main.ino
+++ main.ino
@@ -1,1 +1,6 @@
-//AS5600: https://www.mouser.sg/datasheet/2/588/AS5600_DS000365_5_00-1877365.pdf
+//Main code for SmartDial
+
+//------------------------------Components---------------------------------
+// AS5600: https://www.mouser.sg/datasheet/2/588/AS5600_DS000365_5_00-1877365.pdf
+// ESP32:  https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf
+// 
